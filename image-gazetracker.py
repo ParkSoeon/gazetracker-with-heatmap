d99d2906d37f2ce5e7f8a30da7e2819b1f33cd0d
ParkSoeon <106726934+ParkSoeon@users.noreply.github.com>
2025-03-16 03:21:29 +0900

    Update image-gazetracker.py

--- image-gazetracker.py
+++ image-gazetracker.py
@@ -1,5 +1,5 @@
 import cv2
-import autopy
+import pyautogui
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pyscreenshot
@@ -11,6 +11,7 @@ ESCAPE_KEY = 27
 
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+
 
 def transform_video_coordinates_to_screen(eye_x_pos, eye_y_pos):
     if not video_resolution:
@@ -33,16 +34,12 @@ def update_mouse_position(hough_circles, eye_x_pos, eye_y_pos, roi_color2):
                 center=circle_center,
                 radius=circle[2],
                 color=WHITE,
-                thickness=2
+                thickness=2,
             )
             # print("drawing circle")
             # draw the center of the circle
             cv2.circle(
-                img=roi_color2,
-                center=circle_center,
-                radius=2,
-                color=WHITE,
-                thickness=3
+                img=roi_color2, center=circle_center, radius=2, color=WHITE, thickness=3
             )
 
             # print(i[0],i[1])
@@ -50,29 +47,26 @@ def update_mouse_position(hough_circles, eye_x_pos, eye_y_pos, roi_color2):
             x_pos = int(eye_x_pos)
             y_pos = int(eye_y_pos)
             (x_pos, y_pos) = transform_video_coordinates_to_screen(eye_x_pos, eye_y_pos)
-            autopy.mouse.move(x_pos, y_pos)
+            pyautogui.mouse.move(x_pos, y_pos)
     except Exception as e:
 
-        print('Exception:', e)
+        print("Exception:", e)
 
-face_cascade = cv2.CascadeClassifier(
-    'haarcascades/haarcascade_frontalface_default.xml'
-)
+
+face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 # eye_cascade = cv2.CascadeClassifier(
 #     'haarcascades/haarcascade_eye.xml'
 # )
 
-#testing right eye tracking
-eye_cascade = cv2.CascadeClassifier(
-    'haarcascades/haarcascade_righteye_2splits.xml'
-)
+# testing right eye tracking
+eye_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_righteye_2splits.xml")
 
-#number signifies camera
+# number signifies camera
 video_capture = cv2.VideoCapture(0)
 eye_x_positions = list()
 eye_y_positions = list()
 
-screen_resolution = autopy.screen.size()
+screen_resolution = pyautogui.screen.size()
 print("screen resolution is")
 print(screen_resolution)
 if video_capture.isOpened():
@@ -86,20 +80,20 @@ else:
 while 1:
     success, image = video_capture.read()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    # faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     eyes = eye_cascade.detectMultiScale(gray)
 
-    for (eye_x, eye_y, eye_width, eye_height) in eyes:
+    for eye_x, eye_y, eye_width, eye_height in eyes:
 
         cv2.rectangle(
-            img=image, 
-            pt1=(eye_x, eye_y), 
-            pt2=(eye_x + eye_width, eye_y + eye_height), 
-            color=GREEN, 
-            thickness=2
+            img=image,
+            pt1=(eye_x, eye_y),
+            pt2=(eye_x + eye_width, eye_y + eye_height),
+            color=GREEN,
+            thickness=2,
         )
-        roi_gray2 = gray[eye_y: eye_y + eye_height, eye_x: eye_x + eye_width]
-        roi_color2 = image[eye_y: eye_y + eye_height, eye_x: eye_x + eye_width]
+        roi_gray2 = gray[eye_y : eye_y + eye_height, eye_x : eye_x + eye_width]
+        roi_color2 = image[eye_y : eye_y + eye_height, eye_x : eye_x + eye_width]
 
         hough_circles = cv2.HoughCircles(
             roi_gray2,
@@ -109,7 +103,7 @@ while 1:
             param1=200,
             param2=1,
             minRadius=0,
-            maxRadius=0
+            maxRadius=0,
         )
 
         eye_x_pos = (eye_x + eye_width) / 2
@@ -117,13 +111,12 @@ while 1:
         print(eye_x_pos, eye_y_pos)
         eye_x_positions.append(eye_x_pos)
         eye_y_positions.append(eye_y_pos)
-        
 
         update_mouse_position(hough_circles, eye_x_pos, eye_y_pos, roi_color2)
 
-    cv2.imshow('img', image)
+    cv2.imshow("img", image)
 
-    key_pressed = cv2.waitKey(30) & 0xff
+    key_pressed = cv2.waitKey(30) & 0xFF
     if key_pressed == ESCAPE_KEY:
         break
 
@@ -146,50 +139,52 @@ width, height = background_image.size
 
 print(width, height)
 my_dpi = 118
-#trying contour heatmap
+# trying contour heatmap
 # x = sns.kdeplot(eye_x_positions, eye_y_positions, shade=True, cmap='Reds', shade_lowest=False)
-#trying scatter plot
+# trying scatter plot
 plt.scatter(eye_x_positions, eye_y_positions)
 
-#Comment starts
+# Comment starts
 plt.tight_layout(pad=1)
-plt.axis('off')
-#plt.figure(figsize=(width/my_dpi, height/my_dpi), dpi=my_dpi)
-#plt.figure(figsize=(width, height))
-plt.savefig('scatter.png', dpi=my_dpi*2)
+plt.axis("off")
+# plt.figure(figsize=(width/my_dpi, height/my_dpi), dpi=my_dpi)
+# plt.figure(figsize=(width, height))
+plt.savefig("scatter.png", dpi=my_dpi * 2)
 scatter = PIL.Image.open("scatter.png")
 
 new_size = (width, height)
 scatter = scatter.resize(new_size)
 scatter = scatter.save("new_scatter.png")
 
-img1 = cv2.imread('screenshot.png')
-img2 = cv2.imread('new_scatter.png')
-dst = cv2.addWeighted(img1,0.7,img2,0.3,0)
-cv2.imshow('scatter plot',dst)
-cv2.imwrite('Output_Scatter.png', dst) 
+img1 = cv2.imread("screenshot.png")
+img2 = cv2.imread("new_scatter.png")
+dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
+cv2.imshow("scatter plot", dst)
+cv2.imwrite("Output_Scatter.png", dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 plt.clf()
-#comment ends
+# comment ends
 
 """Adding code for heatmap"""
-x = sns.kdeplot(eye_x_positions, eye_y_positions, shade=True, cmap='rocket_r', shade_lowest=False)
+x = sns.kdeplot(
+    eye_x_positions, eye_y_positions, shade=True, cmap="rocket_r", shade_lowest=False
+)
 plt.tight_layout(pad=1)
-plt.axis('off')
-plt.savefig('heatmap.png', dpi=my_dpi*2)
+plt.axis("off")
+plt.savefig("heatmap.png", dpi=my_dpi * 2)
 heatmap = PIL.Image.open("heatmap.png")
 
 new_size = (width, height)
 heatmap = heatmap.resize(new_size)
 heatmap = heatmap.save("new_heatmap.png")
 
-img1 = cv2.imread('screenshot.png')
-img2 = cv2.imread('new_heatmap.png')
-dst = cv2.addWeighted(img1,0.6,img2,0.4,0)
-cv2.imshow('heatmap plot',dst)
-cv2.imwrite('Output_heatmap.png', dst) 
+img1 = cv2.imread("screenshot.png")
+img2 = cv2.imread("new_heatmap.png")
+dst = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
+cv2.imshow("heatmap plot", dst)
+cv2.imwrite("Output_heatmap.png", dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
